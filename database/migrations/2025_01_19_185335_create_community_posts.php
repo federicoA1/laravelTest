c84fa44e77015e997eac7d175fe1eb98e0ebe4da
Federico <federico.emiliani@bairesdev.com>
2025-01-19 16:01:06 -0300

    feat: livewire and initial migration (tables)

---
+++ database/migrations/2025_01_19_185335_create_community_posts.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('community_posts', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id')->index();
+            $table->string('title');
+            $table->string('content');
+            $table->integer('like_count'); // this will improve performance in the future
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('community_posts');
+    }
+};
